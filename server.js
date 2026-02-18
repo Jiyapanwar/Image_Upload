d3aca82ce3aee7177972ad20ba5448c6302e600a
Jiyapanwar <jiyapanwar2424@gmail.com>
2026-02-18 09:51:33 +0530

    configured env file

--- server.js
+++ server.js
@@ -1,12 +1,16 @@
 import express from "express";
 import mongoose from "mongoose";
+import dotenv from "dotenv";
 import multer from "multer";
 import { v2 as cloudinary } from "cloudinary";
+
 cloudinary.config({
-  cloud_name: "dqxfguygm",
-  api_key: "347723495639414",
-  api_secret: "nxIR0y1LlLEYpaaa6WqDjN2wyq8",
+  cloud_name: process.env.CLOUD_NAME,
+  api_key: process.env.API_KEY,
+  api_secret: process.env.API_SECRET,
 });
+
+dotenv.config();
 
 const app = express();
 
@@ -25,7 +29,8 @@ app.get("/", (req, res) => {
 });
 // jo hume dynamic cheej rakhni hoti hai usko aise curly braces mai we putt and give a intial value
 
-const port = 3000;
+const PORT = process.env.PORT || 3000;
+
 app.listen(port, () => {
   console.log(`Server is running on port ${port}`);
 });
