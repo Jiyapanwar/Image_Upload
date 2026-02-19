45a2356c15b31cab8fe1c49d32ccbc8e07bcd9e7
Jiyapanwar <jiyapanwar2424@gmail.com>
2026-02-19 09:25:54 +0530

    errors handled

--- server.js
+++ server.js
@@ -3,16 +3,17 @@ import mongoose from "mongoose";
 import dotenv from "dotenv";
 import multer from "multer";
 import { v2 as cloudinary } from "cloudinary";
+import path from "path";
 
-cloudinary.config({
-  cloud_name: process.env.CLOUD_NAME,
-  api_key: process.env.API_KEY,
-  api_secret: process.env.API_SECRET,
-});
+const app = express();
 
 dotenv.config();
 
-const app = express();
+cloudinary.config({
+  cloud_name: process.env.CLOUDINARY_CLOUD_NAME,
+  api_key: process.env.CLOUDINARY_API_KEY,
+  api_secret: process.env.CLOUDINARY_API_SECRET,
+});
 
 mongoose
   .connect(
@@ -29,7 +30,42 @@ app.get("/", (req, res) => {
 });
 // jo hume dynamic cheej rakhni hoti hai usko aise curly braces mai we putt and give a intial value
 
-const PORT = process.env.PORT || 3000;
+const storage = multer.diskStorage({
+  //   destination: "./public/uploads",
+
+  filename: function (req, file, cb) {
+    const uniqueSuffix = Date.now() + path.extname(file.originalname);
+    cb(null, file.fieldname + "-" + uniqueSuffix);
+  },
+});
+
+const upload = multer({ storage: storage });
+
+const imageSchema = new mongoose.Schema({
+  filename: String,
+  public_id: String,
+  imgUrl: String,
+});
+const File = mongoose.model("cloudinary", imageSchema);
+
+app.post("/upload", upload.single("file"), async (req, res) => {
+  const file = req.file.path;
+  const cloudinaryRes = await cloudinary.uploader.upload(file, {
+    folder: "NodeJs",
+  });
+
+  const db = await File.create({
+    filename: file.originalname,
+    public_id: cloudinaryRes.public_id,
+    imageUrl: cloudinaryRes.secure_url,
+  });
+
+  res.render("index.ejs", { url: cloudinaryRes.secure_url });
+
+  //   res.json({ message: "file uploaded successfully", cloudinaryRes });
+});
+
+const port = process.env.PORT || 3000;
 
 app.listen(port, () => {
   console.log(`Server is running on port ${port}`);
